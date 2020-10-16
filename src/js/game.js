8a6fd19de7642d7d6fb39cb221c37c2ac4d1355a
Ricardo <ricardolcao@gmail.com>
2020-10-16 08:42:19 +0800

    debuging

--- src/js/game.js
+++ src/js/game.js
@@ -10,7 +10,6 @@ class Game {
         this.playernums = n;
 
         this.publicInfo["round"] = 0;
-        this.publicInfo["turn"] = 0;
         this.publicInfo["richi"] = 0;
         this.publicInfo["honba"] = 0;
 
@@ -32,7 +31,7 @@ class Game {
     initPlayers() {
         for (let i = 0; i < this.playernums; ++i) {
             let playerHandle = document.querySelector(`.player${i}`);
-            let player = new GamePlayer(playerHandle, `player${i}`, 25000, i);
+            let player = new GamePlayer(playerHandle, this.playernums, `player${i}`, 25000, i, this.publicInfo);
 
             this.players.push(player);
         }
@@ -54,7 +53,14 @@ class Game {
 }
 
 class GamePlayer {
-    constructor(handle, name, points, pos) {
+    constructor(handle, playernums, name, points, pos, publicInfo) {
+        this.playernums = playernums;
+        this.initHandles(handle);
+
+        this.initValues(name, points, pos, publicInfo);
+    }
+
+    initHandles(handle) {
         this.handle = handle;
 
         this.HrichiS = this.handle.querySelector(".richi-s");
@@ -70,22 +76,43 @@ class GamePlayer {
         this.Htsumo = this.handle.querySelector(".tsumo");
         this.Hrichi = this.handle.querySelector(".richi");
 
+        this.Hdice.addEventListener("click", (event) => { console.log(`${this.pos} clicked dice.`); });
+        this.Hron.addEventListener("click", (event) => { console.log(`${this.pos} clicked ron.`); });
+        this.Htsumo.addEventListener("click", (event) => { console.log(`${this.pos} clicked tsumo.`); });
+        this.Hrichi.addEventListener("click", (event) => { console.log(`${this.pos} clicked richi.`); });
+    }
+
+    initValues(name, points, pos) {
+        this.richiS = false;
+        this.pos = pos;
+        this.dice = pos === 0;
         this.name = name;
         this.points = points;
-        this.pos = pos;
+        this.round = publicInfo["round"];
+        this.honbaN = publicInfo["honba"];
+        this.richiN = publicInfo["richi"];
     }
 
     set richiS(n) {
+        this.VrichiS = n;
         if (n === true)
             this.HrichiS.classList.remove("hidden");
         else if (n === false)
             this.HrichiS.classList.add("hidden");
     }
 
+    get richiS() {
+        return this.VrichiS;
+    }
+
     set pos(n) {
         this.Vpos = n;
         this.posList = ["dou", "nan", "sei", "hoku"];
         this.Hpos.src = `./static/img/${this.posList[n]}.png`;
+    }
+
+    get pos() {
+        return this.Vpos;
     }
 
     set dice(n) {
@@ -114,7 +141,11 @@ class GamePlayer {
     }
 
     set round(n) {
-        this.Hround.innerHTML = n;
+        this.Vround = n;
+
+        this.roundList = ["东", "南", "西", "北"];
+
+        this.Hround.innerHTML = `${this.roundList[n / this.playernums]}${n % this.playernums + 1}局`;
     }
 
     set honbaN(n) {
