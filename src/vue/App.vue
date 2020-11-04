ba3ca2eae0773b9b3c436703c7e911961cebb86d
Ricardo <ricardolcao@gmail.com>
2020-11-04 20:35:30 +0800

    rewrite app.vue

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -1,183 +1,47 @@
 <template>
     <div class="container flex-center flex-column">
-        <!--<div class="player player0 flex-center flex-column">
-            <div class="richi-s flex-center">
-                <img src="/static/img/richi-s.png" />
-            </div>
-            <div class="flex-center">
-                <div class="left flex-around flex-column">
-                    <div class="pos flex-center">
-                        <img src />
-                    </div>
-                    <div class="dice flex-center">
-                        <img src="/static/img/dice.png" />
-                    </div>
-                </div>
-                <div class="playerinfo">
-                    <div class="points flex-center"></div>
-                    <div class="name flex-center"></div>
-                </div>
-                <div class="publicinfo flex-center flex-column">
-                    <div class="round flex-center"></div>
-                    <div class="honba-n flex-center">
-                        <img src="/static/img/honba.png" />
-                        <div></div>
-                    </div>
-                    <div class="richi-n flex-center">
-                        <img src="/static/img/richi.png" />
-                        <div></div>
-                    </div>
-                </div>
-                <div class="buttons flex-center flex-column">
-                    <button class="ron">ロン</button>
-                    <button class="tsumo">ツモ</button>
-                    <button class="richi">リーチ</button>
-                </div>
-            </div>
-        </div>
-        <div class="player player1 flex-center flex-column">
-            <div class="richi-s flex-center">
-                <img src="/static/img/richi-s.png" />
-            </div>
-            <div class="flex-center">
-                <div class="left flex-around flex-column">
-                    <div class="pos flex-center">
-                        <img src />
-                    </div>
-                    <div class="dice flex-center">
-                        <img src="/static/img/dice.png" />
-                    </div>
-                </div>
-                <div class="playerinfo">
-                    <div class="points flex-center"></div>
-                    <div class="name flex-center"></div>
-                </div>
-                <div class="publicinfo flex-center flex-column">
-                    <div class="round flex-center"></div>
-                    <div class="honba-n flex-center">
-                        <img src="/static/img/honba.png" />
-                        <div></div>
-                    </div>
-                    <div class="richi-n flex-center">
-                        <img src="/static/img/richi.png" />
-                        <div></div>
-                    </div>
-                </div>
-                <div class="buttons flex-center flex-column">
-                    <button class="ron">ロン</button>
-                    <button class="tsumo">ツモ</button>
-                    <button class="richi">リーチ</button>
-                </div>
-            </div>
-        </div>
-        <div class="player player2 flex-center flex-column">
-            <div class="richi-s flex-center">
-                <img src="/static/img/richi-s.png" />
-            </div>
-            <div class="flex-center">
-                <div class="left flex-around flex-column">
-                    <div class="pos flex-center">
-                        <img src />
-                    </div>
-                    <div class="dice flex-center">
-                        <img src="/static/img/dice.png" />
-                    </div>
-                </div>
-                <div class="playerinfo">
-                    <div class="points flex-center"></div>
-                    <div class="name flex-center"></div>
-                </div>
-                <div class="publicinfo flex-center flex-column">
-                    <div class="round flex-center"></div>
-                    <div class="honba-n flex-center">
-                        <img src="/static/img/honba.png" />
-                        <div></div>
-                    </div>
-                    <div class="richi-n flex-center">
-                        <img src="/static/img/richi.png" />
-                        <div></div>
-                    </div>
-                </div>
-                <div class="buttons flex-center flex-column">
-                    <button class="ron">ロン</button>
-                    <button class="tsumo">ツモ</button>
-                    <button class="richi">リーチ</button>
-                </div>
-            </div>
-        </div>
-        <div class="player player3 flex-center flex-column">
-            <div class="richi-s flex-center">
-                <img src="/static/img/richi-s.png" />
-            </div>
-            <div class="flex-center">
-                <div class="left flex-around flex-column">
-                    <div class="pos flex-center">
-                        <img src />
-                    </div>
-                    <div class="dice flex-center">
-                        <img src="/static/img/dice.png" />
-                    </div>
-                </div>
-                <div class="playerinfo">
-                    <div class="points flex-center"></div>
-                    <div class="name flex-center"></div>
-                </div>
-                <div class="publicinfo flex-center flex-column">
-                    <div class="round flex-center"></div>
-                    <div class="honba-n flex-center">
-                        <img src="/static/img/honba.png" />
-                        <div></div>
-                    </div>
-                    <div class="richi-n flex-center">
-                        <img src="/static/img/richi.png" />
-                        <div></div>
-                    </div>
-                </div>
-                <div class="buttons flex-center flex-column">
-                    <button class="ron">ロン</button>
-                    <button class="tsumo">ツモ</button>
-                    <button class="richi">リーチ</button>
-                </div>
-            </div>
-        </div>-->
-        <Player id="player0" name="player0" :pos="0"></Player>
-        <Player id="player1" name="player1" :pos="1"></Player>
-        <Player id="player2" name="player2" :pos="2"></Player>
-        <Player id="player3" name="player3" :pos="3"></Player>
+        <PlayerComp v-for="(item, index) in playerNames" :key="index" :id="`player${index}`" :name="item" :pos="index"></PlayerComp>
 
-        <div class="board flex-center flex-column">
-            <button class="exhaustive">荒牌流局</button>
-            <button class="abortive">途中流局</button>
-            <button class="multiron">多家和</button>
-            <button class="pao">包牌</button>
-            <button class="history">查看历史</button>
-        </div>
+        <ControlComp></ControlComp>
     </div>
 </template>
 
 <script>
-import { onMounted, ref } from "vue";
-import Player from "./Player.vue";
-import game from "../js/game.js";
-import dialog from "../js/dialog.js";
+    import { onMounted, ref } from "vue";
 
-export default {
-    name: "App",
-    components: {Player},
-    setup() {
-        const ins = ref(null);
+    import PlayerComp from "./Player.vue";
+    import ControlComp from "./Control.vue";
 
-        onMounted(async () => {
-            await game.init();
-            game.start();
-            ins.value = new dialog();
-            await ins.value.show();
-            ins.value.show("你好！！");
-        });
+    import game from "../js/game.js";
+    import Dialog from "../js/dialog.js";
 
-        return {
-            ins,
-        };
-    },
-};
+    export default {
+        name: "App",
+        components: { PlayerComp, ControlComp },
+        setup() {
+            const ins = ref(null);
+
+            // 在此弹出 beginning dialog，获取 playerNum、playerNames
+
+            /*const dialog = new Dialog("beginning");
+            const {playerNum, playerNames} = await dialog.show();*/
+
+            let playerNum = 4,
+                playerNames = ["0", "1", "2", "3"];
+
+            /*onMounted(async () => {
+                await game.init();
+                game.start();
+                ins.value = new Dialog();
+                await ins.value.show();
+                ins.value.show("你好！！");
+            });*/
+
+            return {
+                playerNum,
+                playerNames,
+                ins,
+            };
+        },
+    };
 </script>
---
+++ src/vue/Control.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="board flex-center flex-column">
+        <button class="exhaustive">荒牌流局</button>
+        <button class="abortive">途中流局</button>
+        <button class="multiron">多家和</button>
+        <button class="pao">包牌</button>
+        <button class="history">查看历史</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+};
+</script>
+
+<style>
+@media (orientation: landscape) {
+    .board {
+        width: calc(100% / 16 * 9 / 4);
+        height: calc(100% / 9 * 16 / 2);
+        transform: rotate(270deg);
+    }
+}
+
+@media (orientation: portrait) {
+    .board {
+        width: calc(100% / 4);
+        height: calc(100% / 2);
+    }
+}
+
+.board > button {
+    width: 100%;
+    height: 10%;
+    margin: 5% auto;
+}
+</style>
