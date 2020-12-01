f53ea006b206fd2da70dd396828c5d67351d7b0e
Ricardo <ricardolcao@gmail.com>
2020-12-01 15:26:02 +0800

    调整dialog样式

--- src/vue/dialog.vue
+++ src/vue/dialog.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="dialog-wrapper" :class="{ 'is-hidden': !dialogVisible }">
-    <div class="dialog">
-      <div class="dialog-header">{{ dialogData.type }}</div>
-      <div class="dialog-body">
-        {{ dialogData.data }}
-        <!--<Test
+  <div class="dialog" :class="{ 'is-hidden': !dialogVisible }">
+    <div class="dialog-header flex-center">{{ dialogData.type }}</div>
+    <div class="dialog-body">
+      {{ dialogData.data }}
+      <!--<Test
           v-if="dialogData.type === 'test'"
           :value="dialogData.data"
         />-->
-      </div>
-      <div class="dialog-footer">
-        <button @click="cancel">取消</button>
-        <button @click="confirm">确定</button>
-      </div>
+    </div>
+    <div class="dialog-footer flex-center">
+      <button @click="cancel">取消</button>
+      <button @click="confirm">确定</button>
     </div>
   </div>
 </template>
@@ -42,58 +40,37 @@ export default {
 </script>
 
 <style>
-.dialog-wrapper {
+.dialog {
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
-  background-color: rgba(0, 0, 0, 0.6);
-
-  opacity: 1;
+  top: 5%;
+  left: 5%;
+  right: 5%;
+  bottom: 5%;
 
   transition: opacity cubic-bezier(0.215, 0.61, 0.355, 1) 0.3s;
-}
-
-.dialog-wrapper.is-hidden {
-  opacity: 0;
-  pointer-events: none;
-}
-
-.dialog {
-  max-width: 600px;
-
-  margin: auto;
-  margin-top: 30vh;
-  padding: 24px;
-
-  border-radius: 4px;
-
-  background-color: white;
-
   transition: transform cubic-bezier(0.215, 0.61, 0.355, 1) 0.3s;
 }
 
-.is-hidden .dialog {
+.dialog.is-hidden {
+  opacity: 0;
+  pointer-events: none;
   transform: translateY(-10px);
 }
 
 .dialog-header {
-  margin-bottom: 24px;
+  height: 15%;
+}
+
+.dialog-body {
+  height: 70%;
 }
 
 .dialog-footer {
-  text-align: right;
-  margin-top: 24px;
+  height: 15%;
 }
 
-.dialog-footer button + button {
-  margin-left: 16px;
-}
-
-.dialog button {
-  padding: 4px 8px;
+.dialog-footer button {
+  margin: 0 5%;
   background: none;
   border: none;
   outline: none;
